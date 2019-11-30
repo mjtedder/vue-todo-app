5b914b7e75b15d6d4e24241f8a9b1e776eeff415
Matt Tedder <mtedder@Techtonics-MacBook-Pro-2.local>
2019-11-30 13:28:01 -0700

    creates AddTodo component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <div id="app">
     <Header />
+    <AddTodos v-on:add-todo="addTodo"/>
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
 import Todos from "./components/Todos.vue"
-import Header from "./components/layouts/Header.vue"
+import Header from "./components/layouts/Header"
+import AddTodos from "./components/AddTodo";
+
 
 export default {
   name: "app",
   components: {
     Todos,
+    AddTodos,
     Header
   },
   data() {
@@ -39,18 +43,43 @@ export default {
   methods: {
     deleteTodo(id) {
       this.todos = this.todos.filter(todo => todo.id !== id)
+    },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
     }
   }
 };
 </script>
 
 <style>
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
+  line-height: 1.4;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
+}
+
 </style>
