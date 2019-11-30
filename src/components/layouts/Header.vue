c7a9c2ad0e81ae5f3afdbc31323bd86180839ea1
Matt Tedder <mtedder@Techtonics-MacBook-Pro-2.local>
2019-11-30 12:32:03 -0700

    creates Header component in layouts

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -1,5 +1,28 @@
 <template>
-    <header class="header">
-
-    </header>
+  <header class="header">
+    <img alt="Vue logo" src="../../assets/logo.png" />
+    <h1>Vue.js Todo List</h1>
+  </header>
 </template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped>
+img {
+    size: 100px;
+}
+.header {
+  background: #333;
+  color: #fff;
+  text-align: center;
+  padding: 10px;
+}
+.header a {
+  color: #fff;
+  padding-right: 5px;
+}
+</style>
